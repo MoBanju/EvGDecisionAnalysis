096ed9e2db3e07410ea641716f65252da81a389c
Mussa <mussa80banjai@gmail.com>
2024-07-16 18:41:28 +0200

    feat: start of the project

---
+++ EvGDecisionAnalysis/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EvGDecisionAnalysis.Models;
+
+namespace EvGDecisionAnalysis.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<NPVResult> NPVResults { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=npvcalculator.db");
+        }
+    }
+}
---
+++ EvGDecisionAnalysis/View/UserControls/InputBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace EvGDecisionAnalysis.View.UserControls
+{
+    public partial class InputBox : UserControl
+    {
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        private string label;
+        public string Label
+        {
+            get { return label; }
+            set
+            {
+                label = value;
+                tbLabel.Text = label;
+            }
+        }
+
+        public double Value
+        {
+            get { return double.TryParse(valInput.Text, out double result) ? result : 0; }
+            set { valInput.Text = value.ToString(); }
+        }
+    }
+}
